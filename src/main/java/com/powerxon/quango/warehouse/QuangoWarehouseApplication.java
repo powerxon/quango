1c3146f0bc12f4c7856e96b4cc61c29e5f2d36ed
matteoponzini <matteoponzini99@gmail.com>
2020-10-24 23:04:37 +0200

    started project

---
+++ src/main/java/com/powerxon/quango/warehouse/QuangoWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.powerxon.quango.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuangoWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuangoWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/powerxon/quango/warehouse/QuangoWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerxon.quango.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuangoWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
